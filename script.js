fcf6c9dc3860943926907f65ae95638028bf4ddd
DMiteE <64609998+DMiteE@users.noreply.github.com>
2023-07-03 11:29:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-window.addEventListener("scroll", function(){
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', this.window.scrollY > 0);
-});
+const hamburger = document.querySelector(".hamburger"),
+      menu = document.querySelector(".header__menu");
+const center = [52.61653510734731,39.534916969649224];
 
-function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        center: [52.61653510734731,39.534916969649224],
-        zoom: 17
-    });
 
-    myMap.controls.remove('searchControl'); // удаляем поиск
-    myMap.controls.remove('trafficControl'); // удаляем контроль трафика
-    myMap.controls.remove('typeSelector'); // удаляем тип
-    myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    myMap.controls.remove('rulerControl'); // удаляем контрол правил
-  
-}
-ymaps.ready(init);
+hamburger.addEventListener("click" , () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
 
 const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
@@ -27,12 +16,36 @@ const swiper = new Swiper('.swiper', {
       delay: 2500,
       disableOnInteraction: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
   });
+
+  function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        center: center,
+        zoom: 17
+    });
+    let placemark = new ymaps.Placemark(center
+        ,{
+            balloonContent: `
+            
+            <div class="balloon"›
+                <div class="balloon address"›г.Липецк,Поперечный проезд 11А‹/div›
+                <div class="balloon_ contacts"›
+                <a href="tel: +7 (4742) 50-52-52"›+7 (4742) 50-52-52</a> 
+                </div>
+            </div>
+            `
+        },{
+    })
+
+    myMap.controls.remove('searchControl'); // удаляем поиск
+    myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+    myMap.controls.remove('typeSelector'); // удаляем тип
+    myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    myMap.controls.remove('rulerControl'); // удаляем контрол правил
+
+
+    myMap.geoObjects.add(placemark);
+  
+}
+ymaps.ready(init);
